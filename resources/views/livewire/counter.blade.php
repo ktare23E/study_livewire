47ab42eebfa569bbf6f2e901e584db9da1805012
Kristian <kristiankharl.tare@nmsc.edu.ph>
2025-01-10 12:23:31 +0800

    livewire component

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -3,7 +3,7 @@
     Count: {{$count}}
     <div class="space-x-2">
         <button wire:click="increment(2)">+</button>
-        <button wire:click="alert('Hello World')">-</button>
+        <button wire:click="decrement">-</button>
         {{-- <button wire:click='triggerAlert' class="py-1 px-2 bg-blue-500 rounded-sm text-sm text-white">alert</button> --}}
         <button wire:click='swal' class="py-1 px-2 bg-blue-500 rounded-sm text-sm text-white">sweet alert</button>
     </div>
