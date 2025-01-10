69b3688b2d30bd9468af0b8c6bf899ba8c4d8dde
Kristian <kristiankharl.tare@nmsc.edu.ph>
2025-01-10 11:26:17 +0800

    learning about mount and updated lifecycle hooks

--- app/Livewire/Todos.php
+++ app/Livewire/Todos.php
@@ -8,16 +8,27 @@ class Todos extends Component
 {
     public $todo = '';
 
-    public $todos = [
-        'Take out trash',
-        'Do dishes'
-    ];
+    public $todos = [];
 
     public function add(){
         array_push($this->todos,$this->todo);
-        `
         // $this->todo = '';
         $this->reset('todo');
+    }
+
+    // public function updated($property,$values){
+    //     $this->$property = strtoupper($values);
+    // }
+
+    public function updatedTodo($value){
+        $this->todo = strtoupper($value);
+    }
+
+    public function mount(){
+        $this->todos = [
+            'Take out trash',
+            'Do dishes'
+        ];
     }
     
     public function render()
